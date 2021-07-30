c35983579f21c55bcf218f17b910a2121cad2557
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2021-07-30 10:51:46 +0300

    Add button to notes component to reach create component.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,8 +1,10 @@
-import { Container, Grid, Paper } from "@material-ui/core";
+import { Container, Grid, Paper, Button } from "@material-ui/core";
 import { useState, useEffect } from "react";
+import { useHistory } from 'react-router-dom';
 
 function Notes() {
   const [notes, setNotes] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     fetch("http://localhost:8000/notes")
@@ -20,6 +22,16 @@ function Notes() {
             </Paper>
           </Grid>
         ))}
+        <Grid item xs={12} sm={12} md={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={() => history.push("/create")}
+          >
+            New note
+          </Button>
+        </Grid>
       </Grid>
     </Container>
   );
